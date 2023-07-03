8f334b75ba2f04a5d7251d1832c32dcbb1618335
Tmob <mrrtmob@gmail.com>
2023-07-03 13:53:56 +0700

    first commit

--- test/BrixieToken.js
+++ test/BrixieToken.js
@@ -47,7 +47,6 @@ describe("BrixieToken contract", function() {
       expect(addr1Balance).to.equal(50);
 
       // Transfer 50 tokens from addr1 to addr2
-      // We use .connect(signer) to send a transaction from another account
       await brixieToken.connect(addr1).transfer(addr2.address, 50);
       const addr2Balance = await brixieToken.balanceOf(addr2.address);
       expect(addr2Balance).to.equal(50);
@@ -56,7 +55,6 @@ describe("BrixieToken contract", function() {
     it("Should fail if sender doesn't have enough tokens", async function () {
       const initialOwnerBalance = await brixieToken.balanceOf(owner.address);
       // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
-      // `require` will evaluate false and revert the transaction.
       await expect(
         brixieToken.connect(addr1).transfer(owner.address, 1)
       ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
