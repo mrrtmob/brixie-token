c69970b3339589796ff842f652ce355d08b71747
Tmob <mrrtmob@gmail.com>
2023-07-03 15:08:02 +0700

    transaction

--- transaction/send.js
+++ transaction/send.js
@@ -4,8 +4,9 @@ const web3 = new Web3("http://13.212.180.250:8545/");
 
 var jsonFile = "abi.json";
 var parsed = JSON.parse(fs.readFileSync(jsonFile));
-var abi = parsed.abi;
-const signer = web3.eth.accounts.privateKeyToAccount('0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d');
+const signer = web3.eth.accounts.privateKeyToAccount(
+  "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+);
 web3.eth.accounts.wallet.add(signer);
 var contract = new web3.eth.Contract(
   parsed,
@@ -13,13 +14,18 @@ var contract = new web3.eth.Contract(
   { from: signer.address }
 );
 
-
-async function send(to,amount) {
-    contract.methods.transfer(to,amount).send({
-        from: signer.address,
-        gas: 5000000
-    }).then(console.log).catch(console.error);
+async function send({ to, amount }) {
+  contract.methods
+    .transfer(to, amount)
+    .send({
+      from: signer.address,
+      gas: 5000000,
+    })
+    .then(console.log)
+    .catch(console.error);
 }
 
-
-send('0x714d422a63D556042DA8Eca269f63337290612Eb', "9999999999999999999999900")
+send({
+  to: "0x714d422a63D556042DA8Eca269f63337290612Eb",
+  amount: "9999999999999999999999900",
+});
